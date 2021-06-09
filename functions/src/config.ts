5e4bce178172a87d314d776d5978e95e73797cbb
Fausto Blanco <faustoblanco@bemol.com.br>
2021-06-09 07:33:25 -0300

    change firebase project

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -3,9 +3,6 @@ export const UPLOAD_PATH = 'uploads';
 export const SHARE_PATH = 'share';
 export const ALLOWED_HOSTS = [
   'localhost:5001',
-  'io-photobooth-dev.web.app',
-  'io-photo-booth.web.app',
-  'us-central1-io-photobooth-dev.cloudfunctions.net',
-  'us-central1-io-photo-booth.cloudfunctions.net',
-  'photobooth.flutter.dev',
+  'photobooth-extended-io-b-e0a2b.web.app',
+  'southamerica-east1-photobooth-extended-io-b-e0a2b.cloudfunctions.net',
 ];
