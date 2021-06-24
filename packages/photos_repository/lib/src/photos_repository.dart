dd9aeea2e0f0c8b22b324c5146a782613c2d4c30
Tacio Medeiros <taciomedeiros@MacBook-Vitta.local>
2021-06-24 20:12:58 -0300

    fix: alteracao de url para share

--- packages/photos_repository/lib/src/photos_repository.dart
+++ packages/photos_repository/lib/src/photos_repository.dart
@@ -56,7 +56,7 @@ class ShareUrls {
   final String twitterShareUrl;
 }
 
-const _shareUrl = 'https://photobooth-extended-io-b-e0a2b.web.app/share';
+const _shareUrl = 'https://photobooth.gdg.rocks/share';
 
 /// {@template photos_repository}
 /// Repository that persists photos in a Firebase Storage.
